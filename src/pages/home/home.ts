9987e6e0a908439de5aeed9f2f44c804294ae197
shlee3 <shlee@satreci.com>
2017-11-09 16:44:57 +0900

    froala editor 삽입

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  options: Object = {
+    placeholderText: 'write your a ordinary day'
+  };
+  form = new FormGroup({
+    formModel: new FormControl('', Validators.required),
+  });
+  storage;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private storage: Storage) {
+    this.storage = storage
   }
 
+  onSubmit(event) {
+    storage.set('name', 'Max');
+
+    storage.get('age').then((val) => {
+      console.log('Your age is', val);
+    });
+    console.info('this.form.value ... ', this.form.value.formModel);
+  }
 }
