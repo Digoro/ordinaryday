cb1aebb451887cee9daf77a5f0718f5d693cc758
shlee <lsh00124@naver.com>
2017-11-10 22:39:16 +0900

    diary 예시

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,18 +12,42 @@ export class HomePage {
   form = new FormGroup({
     formModel: new FormControl('', Validators.required),
   });
-  storage;
 
-  constructor(public navCtrl: NavController, private storage: Storage) {
-    this.storage = storage
+  diaries = [{
+    id: 0,
+    date: new Date("2017-11-03"),
+    content: "1일차"
+  },{
+    id: 1,
+    date: new Date("2017-11-04"),
+    content: "2일차"
+  },{
+    id: 2,
+    date: new Date("2017-11-05"),
+    content: "3일차"
+  },{
+    id: 3,
+    date: new Date("2017-11-06"),
+    content: "4일차"
+  }];
+
+  constructor(public navCtrl: NavController) {
   }
 
   onSubmit(event) {
-    storage.set('name', 'Max');
-
-    storage.get('age').then((val) => {
-      console.log('Your age is', val);
+    this.diaries.push({
+      id: this.diaries.length,
+      date: new Date().toString(),
+      content: this.form.value.formModel
     });
-    console.info('this.form.value ... ', this.form.value.formModel);
+    this.form.value.formModel = '';
+  }
+
+  delete(diary) {
+    console.log(diary);
+    this.diaries.filter(function (d) {
+      return d.id != diary.id;
+    });
+    console.log(this.diaries)
   }
 }
